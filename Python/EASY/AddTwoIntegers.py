10ab1690400e0abb0d291a83499b55e9e69c5062
tagaritt <47823969+tagaritt@users.noreply.github.com>
2025-05-17 12:15:54 -0400

    Solution presented for LeetCode's "Add Two Integers" problem.
    Script includes LeetCode's description of the task, as well as my solution proposal and implementation.

---
+++ Python/EASY/AddTwoIntegers.py
@@ -0,0 +1,27 @@
+class Solution(object):
+  def sum(self, num1, num2):
+    """
+    TASK:
+    Given two integers 'num1' and 'num2', return the sum of the two integers.
+
+    VARIABLE TYPES:
+    :type num1:  int
+    :type num2:  int
+    :rtype:      int
+
+    EXAMPLE 1:
+      Input:       num1 = 12, num2 = 5
+      Output:      17
+      Explanation: num1 is 12, num2 is 5, and their sum is 12 + 5 = 17, so 17 is returned.
+
+    EXAMPLE 2:
+      Input:       num1 = -10, num2 = 4
+      Output:      -6
+      Explanation: num1 + num2 = -6, so -6 is returned.
+    
+    IMPLEMENTATION PROPOSAL:
+    Create a simple solution within the return statement where the two input variables are added directly and the sum is the only thing returned.
+    No need to overcomplicate, just complete the task.
+    """
+
+    return num1 + num2
